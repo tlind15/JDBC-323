4f00d697f0fef67397c8692f38c87b8cc8dc5ee4
tlind15 <tlind15@gmail.com>
2016-10-12 12:12:12 -0700

    Delete connect function. Start addBook function.
    connect will not work because all of the functions need access to the Connection variable to send statements to Derby

--- JDBC/src/jdbc/DbFunctions.java
+++ JDBC/src/jdbc/DbFunctions.java
@@ -7,10 +7,13 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,115 +21,51 @@ import java.util.Scanner;
  */
 public class DbFunctions {
     
-      //  Database credentials
-    static String DBNAME; // = JDBC
-    static String USER; // = cecs
-    static String PASS; // = 323
-    
-    //This is the specification for the printout that I'm doing:
-    //each % denotes the start of a new field.
-    //The - denotes left justification.
-    //The number indicates how wide to make the field.
-    //The "s" denotes that it's a string.  All of our output in this test are 
-    //strings, but that won't always be the case.
-    static final String displayFormat="%-5s%-15s%-15s%-15s\n";
-// JDBC driver name and database URL
-    static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";
-    static String DB_URL = "jdbc:derby://localhost:1527/";
-//            + "testdb;user=";
-    
-/**
- * Takes the input string and outputs "N/A" if the string is empty or null.
- * @param input The string to be mapped.
- * @return  Either the input string or "N/A" as appropriate.
- */
-    
+      
+      
     DbFunctions() {} //default empty constructor
     
 /*****Functions for Database Connectivity*****/
-public static String dispNull (String input) {
-        //because of short circuiting, if it's null, it never checks the length.
-        if (input == null || input.length() == 0)
-            return "N/A";
-        else
-            return input;
-    }    
+
     
    public void connect() {
-        //Prompt the user for the database name, and the credentials.
-        //If your database has no credentials, you can update this code to 
-        //remove that from the connection string.
-        Scanner in = new Scanner(System.in);
-        System.out.print("Name of the database (not the user account): ");
-        DBNAME = in.nextLine();
-        System.out.print("Database user name: ");
-        USER = in.nextLine();
-        System.out.print("Database password: ");
-        PASS = in.nextLine();
-        //Constructing the database URL connection string
-        DB_URL = DB_URL + DBNAME + ";user="+ USER + ";password=" + PASS;
-        Connection conn = null; //initialize the connection
-        Statement stmt = null;  //initialize the statement that we're using
-        try {
-            //STEP 2: Register JDBC driver
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
-
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL);
-
-            //STEP 4: Execute a query
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-            String sql;
-            sql = "SELECT * FROM WRITINGGROUPS";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            //STEP 5: Extract data from result set
-            System.out.printf(displayFormat, "ID", "First Name", "Last Name", "Phone #");
-            while (rs.next()) {
-                //Retrieve by column name
-                String id = rs.getString("groupname");
-                String phone = rs.getString("headwriter");
-                String first = rs.getString("yearformed");
-                String last = rs.getString("subject");
-
-                //Display values
-                System.out.printf(displayFormat, 
-                        dispNull(id), dispNull(first), dispNull(last), dispNull(phone));
-            }
-            //STEP 6: Clean-up environment
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            //Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) {
-                    stmt.close();
-                }
-            } catch (SQLException se2) {
-            }// nothing we can do
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!"); 
+       
    }
  //**********
+   
+   public void addBook(Connection conn) throws SQLException {
+       Scanner in = new Scanner(System.in);
+       String stmt = "";
+       PreparedStatement pstmt;
+       ResultSet rs;       
+       
+       System.out.println("What is the title of the book? ");
+       String Title = in.nextLine();
+       
+       System.out.println("How many pages is the book? ");
+       String pages = in.nextLine();
+       
+       System.out.println("What is the name of the writing group who wrote the book? ");
+       String GroupName = in.nextLine();
+       
+       stmt = "Select * FROM writinggroups WHERE groupname = ?";
+       pstmt = conn.prepareStatement(stmt);
+       pstmt.setString(1, GroupName);     
+       rs = pstmt.executeQuery();
+        
+       if(rs.getString("groupname") == null || !rs.getString("groupname").equalsIgnoreCase(GroupName)) 
+       System.out.println("Who is the head writer? ");
+       String HeadWriter = in.nextLine();
+       
+       System.out.println("What year was the writing group formed? ");
+       String YearFormed = in.nextLine();
+       
+       System.out.println("What kinds of books does this group write? ");
+       String Subject = in.nextLine();
+       
+       
+   }
 
    
     
 }
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Status Help
