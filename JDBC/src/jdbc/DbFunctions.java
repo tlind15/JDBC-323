d77875abe40a74154c809ff5eb67ea9989410a60
tlind15 <tlind15@gmail.com>
2016-10-16 15:01:10 -0700

    Add publisher update function
    function is able to create a new publisher. Then it takes all of the books by one of the current publishers and changes them to be published by the new publisher.

--- JDBC/src/jdbc/DbFunctions.java
+++ JDBC/src/jdbc/DbFunctions.java
@@ -11,15 +11,12 @@ package group_writing;
  */
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DbFunctions {
     
@@ -28,7 +25,7 @@ public class DbFunctions {
     DbFunctions() {} //default empty constructor
     
   //function allows user to add a new book to the database  
-   public void addBook(Connection conn) throws SQLException {
+   public void addBook(Connection conn) {
        /*Add a book to the database written by an existing writing group
          and published by an existing publisher*/
        
@@ -36,7 +33,7 @@ public class DbFunctions {
        Scanner in = new Scanner(System.in);
        String stmt = "";
        ArrayList<String> groups = new ArrayList<String> ();
-       PreparedStatement pstmt;
+       PreparedStatement pstmt = null;
        ResultSet rs; 
        
        System.out.println("\nAdd a Book \n");
@@ -51,6 +48,7 @@ public class DbFunctions {
        int pages = Integer.valueOf(in.nextLine()); //in.nextLine returns a string a we want pages as an integer
        //******
        
+       try {
        //***Get groupname*** 
        //must pick a name that already exists in the database
        
@@ -138,17 +136,44 @@ public class DbFunctions {
        
        if (decision.equalsIgnoreCase("y")){
            addBook(conn);
-           return;
        }
-       //******    
-  }
+       //******
+       
+       rs.close();
+       pstmt.close();
+       conn.close();
+       
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+            return;
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+            return;
+        } finally {
+            //finally block used to close resources
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException se2) {}// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }
+   }
    
-   public void removeBook(Connection conn) throws SQLException {
+   public void removeBook(Connection conn) {
        
        Scanner in = new Scanner(System.in);
        String stmt = "";
        ArrayList<String> groups = new ArrayList<String> ();
-       PreparedStatement pstmt;
+       PreparedStatement pstmt = null;
        ResultSet rs;  
        
        System.out.println("\nRemove a Book \n");
@@ -158,6 +183,7 @@ public class DbFunctions {
        String Title = in.nextLine();
        //******
        
+       try {
        //***Get groupname*** 
        //must pick a name that already exists in the database
        
@@ -191,7 +217,7 @@ public class DbFunctions {
        
        pstmt.executeUpdate();
         //******
-        
+       
        //***Allow user to repeat action if they would like
        System.out.println("Deleted!");
        System.out.print("Would you like to remove another book? Enter 'y' or 'n'. ");
@@ -199,7 +225,137 @@ public class DbFunctions {
        
        if (decision.equalsIgnoreCase("y")){
            removeBook(conn);
-           return;
-       }      
-   }   
+           
+       } 
+       
+       rs.close();
+       pstmt.close();
+       conn.close();
+       
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+            return;
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+            return;
+        } finally {
+            //finally block used to close resources
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException se2) {}// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }
+   } 
+   
+   public void publisherChange(Connection conn) {
+       Scanner in = new Scanner(System.in);
+       String stmt = "";
+       ArrayList<String> groups = new ArrayList<String> ();
+       PreparedStatement pstmt = null;
+       ResultSet rs = null;  
+       
+       System.out.println("\nPublisher Update\n");
+       
+       try {
+           //***Get new publisher info
+           System.out.print("What is the new publisher's name? ");
+           String newPub = in.nextLine();
+           
+           System.out.print("What is the new publisher's address? ");
+           String pubAddress = in.nextLine();
+           
+           System.out.print("What is the new publisher's phone? Enter as XXX-XXX-XXXX: ");
+           String pubPhone = in.nextLine();
+           
+           System.out.print("What is the new publisher's email? ");
+           String pubEmail = in.nextLine();
+          
+           
+            //***Get old publisher name*** 
+       
+            //Querying for the list of publishers for the user to choose from
+            stmt = "Select publishername FROM publishers";
+            pstmt = conn.prepareStatement(stmt);     
+            rs = pstmt.executeQuery();
+       
+            //put the groupnames in a list
+            groups.clear();
+            while (rs.next())
+                groups.add(rs.getString("publishername"));
+
+            System.out.println("Who is the old publisher?");
+
+            //display group name choices as options 1, 2, 3, etc.
+            for (int i=0; i < groups.size(); i++) {
+                System.out.println("   " + String.valueOf(i+1) + ") " + groups.get(i));
+            }  
+       
+       System.out.print("Enter the number corresponding to an option above: ");
+       int choice = Integer.valueOf(in.next());
+       //eventually need to account for invalid choice
+       
+       String oldPub = groups.get(choice - 1); //set publishername based on user choice
+       //******
+       
+       //***Create the new publisher using all of the same info from the old publisher but just using the new name***
+            stmt = "INSERT INTO publishers (publishername, publisheraddress, publisherphone, publisheremail)"
+                    + " VALUES (?, ?, ?, ?)";
+
+            pstmt = conn.prepareStatement(stmt);
+            pstmt.setString(1, newPub);
+            pstmt.setString(2, pubAddress);
+            pstmt.setString(3, pubPhone);
+            pstmt.setString(4, pubEmail);
+
+            pstmt.executeUpdate();
+            //******
+      
+       
+       //***Change books by oldPub to be published by newPub
+       stmt = "UPDATE books SET publishername = ? WHERE publishername = ?";
+       pstmt = conn.prepareStatement(stmt);
+       pstmt.setString(1, newPub);
+       pstmt.setString(2, oldPub);
+       
+       pstmt.executeUpdate();
+       //******  
+       
+       rs.close();
+       pstmt.close();
+       conn.close();
+       
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+            return;
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+            return;
+        } finally {
+            //finally block used to close resources
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException se2) {}// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }
+   }
 }
